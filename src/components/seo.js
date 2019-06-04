9860ad3e1b3cebbc4823b7bc9b303e377f719b99
Haichao <haichaozhu77@gmail.com>
2019-06-04 17:26:03 +0800

    add favicon to helmet

--- src/components/seo.js
+++ src/components/seo.js
@@ -9,6 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
+import favicon from "../img/favicon.ico"
 
 function SEO({ description, lang, meta, title }) {
   const { site } = useStaticQuery(
@@ -68,6 +69,11 @@ function SEO({ description, lang, meta, title }) {
           content: metaDescription,
         },
       ].concat(meta)}
+      link={[
+        { rel: "icon", type: "image/gif", sizes: "16x16", href: `${favicon}` },
+        { rel: "icon", type: "image/gif", sizes: "32x32", href: `${favicon}` },
+        { rel: "shortcut icon", type: "image/gif", href: `${favicon}` },
+      ]}
     />
   )
 }
